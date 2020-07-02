6583ff5b092f425feefcba4a08171133e1a538d9
mdm106 <mads.mcmahon@googlemail.com>
2020-07-02 10:43:35 +0100

    Added comments

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ echo $response->getHeaderLine('content-type')."\n"; // 'application/json; charse
 $body = $response->getBody();
 echo $body."\n";
 
-$person = json_decode($body);
-$name = $person->name;
+$person = json_decode($body); //decode response into PHP object
+$name = $person->name; 
 $homeURL = $person->homeworld;
 
-$homePlanet = json_decode($client->request('GET', $homeURL)->getBody())->name;
+$homePlanet = json_decode($client->request('GET', $homeURL)->getBody())->name; //GET request with URL from the homeworld key of person
 
-$firstFilm = json_decode($client->request('GET', $person->films[0])->getBody())->title;
+$firstFilm = json_decode($client->request('GET', $person->films[0])->getBody())->title; //GET request with URL from the first value of the films key of person
 
-echo $name."'s home planet is ".$homePlanet.". He first appeared in ".$firstFilm;
+echo $name."'s home planet is ".$homePlanet.". He first appeared in ".$firstFilm; //results echoed in sentence
 
